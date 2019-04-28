bbc93d22636f99366c993755e1f0a74fc1b8f5d2
Nao Okada <naookada@Naos-MacBook-Pro-2.local>
2019-04-28 16:47:29 +0900

    Edit view for form and index of posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.fetch(:post, {})
+      params.permit(:post, :image)
     end
 end
